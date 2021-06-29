0d969e6db995707e62647eacb2da0e6ceab24d17
German <german.herrera@gesintel.cl>
2021-06-29 12:42:59 -0400

    remove key attribute

--- src/pages/Service/components/SectionEdit/components/FieldSetEdit/FieldSetEdit.js
+++ src/pages/Service/components/SectionEdit/components/FieldSetEdit/FieldSetEdit.js
@@ -120,11 +120,10 @@ const FieldSetEdit = ({ hasHeader=true, section, fieldset, refreshSection }) => 
         { section.type !== 'HEADER' && section.type !== 'CONTACT' && fieldset.fields &&
           <>
           <Row className="titles-section">
-            <Col span={8} offset={1}>Título</Col>
-            <Col span={4}>Clave</Col>
-            <Col span={4}>Tipo</Col>
-            <Col span={3} className="center">Requerido</Col>
-            <Col span={3} className="center">Tabla Visible</Col>
+            <Col span={9} offset={1}>Título</Col>
+            <Col span={5}>Tipo</Col>
+            <Col span={4} className="center">Requerido</Col>
+            <Col span={4} className="center">Tabla Visible</Col>
             <Col span={1} className="center"></Col>
           </Row>
           { fieldset.fields.map((field, index) =>
@@ -134,19 +133,18 @@ const FieldSetEdit = ({ hasHeader=true, section, fieldset, refreshSection }) => 
                   <Button icon="plus" size="small" onClick={addField}/> 
                 }
               </Col>
-              <Col span={8}><Input value={field.title} placeholder="Ingrese nombre del atributo" size="small" onChange={(e) => handleChangeAttribute(index, 'title', e.target.value)}/></Col>
-              <Col span={4}><Input value={field.key} placeholder="Ingrese clave del atributo para JSON" size="small" onChange={(e) => handleChangeAttribute(index, 'key', e.target.value)}/></Col>
-              <Col span={4}>
+              <Col span={9}><Input value={field.title} placeholder="Ingrese nombre del atributo" size="small" onChange={(e) => handleChangeAttribute(index, 'title', e.target.value)}/></Col>
+              <Col span={5}>
                 <Select value={field.typeField} onChange={(value) => handleChangeAttribute(index, 'typeField', value)} size="small">
                   <Select.Option value="INPUT">Texto</Select.Option>
                   <Select.Option value="DATE">Fecha</Select.Option>
                   <Select.Option value="SELECT">Desplegable</Select.Option>
                 </Select>
               </Col>
-              <Col span={3} className="center">
+              <Col span={4} className="center">
                   <Checkbox checked={field.required === true} onChange={(e) => handleChangeAttribute(index, 'required', e.target.checked)} size="small"/>
               </Col>
-              <Col span={3} className="center">
+              <Col span={4} className="center">
                   <Checkbox checked={field.tableVisible === true} onChange={(e) => handleChangeAttribute(index, 'tableVisible', e.target.checked)} size="small"/>
               </Col>
               <Col span={1} className="center">
