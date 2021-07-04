b99a4862e5b948e7ed09c31b2ee59b803b81fc53
German <german.herrera@gesintel.cl>
2021-07-04 19:39:31 -0400

    icono

--- src/pages/Design/components/SectionEdit/components/Catalogo/Catalogo.js
+++ src/pages/Design/components/SectionEdit/components/Catalogo/Catalogo.js
@@ -37,7 +37,12 @@ const Catalogo = ({ catalogo, handleChangeCatalogoActive, handlerChangeCatalogo 
             { catalogo.fields.map((field, index) =>
                 <Col span={6} className={ catalogo.active ? "cat show " : "cat hide "}>
                     <Row className="field-cat">
-                        <Col span={12}>{field.title}</Col>
+                        <Col span={2} className="field-type">
+                            <Tooltip title={field.type === 'INPUT' ? 'Texto editable' : field.type === 'SELECT' ? 'Desplegable' : 'Fecha'}>
+                                <Icon size="small" type={field.type === 'INPUT' ? 'edit' : field.type === 'SELECT' ? 'unordered-list' : 'calendar'}/>
+                            </Tooltip>
+                        </Col>
+                        <Col span={11}>{field.title}</Col>
                         <Col span={4}>
                             <Tooltip title="Activar"><Checkbox checked={field.active} size="small" disabled={!catalogo.active} onChange={(e) => handlerChangeCatalogoField(field, 'active', e.target.checked)} /></Tooltip>
                         </Col>
@@ -46,11 +51,6 @@ const Catalogo = ({ catalogo, handleChangeCatalogoActive, handlerChangeCatalogo 
                         </Col>
                         <Col span={3}>
                             <Tooltip title="Prellenado"><Checkbox checked={field.active && field.prefilled} disabled={!catalogo.active || !field.active} size="small" onChange={(e) => handlerChangeCatalogoField(field, 'prefilled', e.target.checked)} /></Tooltip>
-                        </Col>
-                        <Col span={1}>
-                            <Tooltip title={field.type === 'INPUT' ? 'Texto editable' : field.type === 'SELECT' ? 'Desplegable' : 'Fecha'}>
-                                <Icon size="small" type={field.type === 'INPUT' ? 'edit' : field.type === 'SELECT' ? 'unordered-list' : 'calendar'}/>
-                            </Tooltip>
                         </Col>
                     </Row>
                 </Col>
