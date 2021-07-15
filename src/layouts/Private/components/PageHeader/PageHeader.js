4ab80ac4efae0a280075431d0221ecec27c5a1f8
German <german.herrera@gesintel.cl>
2021-07-15 00:34:37 -0400

    header

--- src/layouts/Private/components/PageHeader/PageHeader.js
+++ src/layouts/Private/components/PageHeader/PageHeader.js
@@ -5,12 +5,12 @@ import { Breadcrumbs } from '../'
 
 export default ({ title, breadcrumbs }) => (
   <Row className="page-header">
-    <Col xs={24} sm={24} md={breadcrumbs && breadcrumbs.length === 2 ? 18 : 4} lg={breadcrumbs && breadcrumbs.length === 2 ? 19 : 4}>
+    <Col xs={24} sm={24} md={breadcrumbs && breadcrumbs.length === 2 ? 18 : 4} lg={breadcrumbs && breadcrumbs.length === 2 ? 18 : 4}>
       { breadcrumbs &&
           <Breadcrumbs items={ breadcrumbs } />
       }
     </Col>
-    <Col xs={24} sm={24} md={breadcrumbs && breadcrumbs.length === 2 ? 6 : 20} lg={breadcrumbs && breadcrumbs.length === 2 ? 5 : 16} style={{textAlign: breadcrumbs && breadcrumbs.length === 2 ? 'right' : 'center'}}>
+    <Col xs={24} sm={24} md={breadcrumbs && breadcrumbs.length === 2 ? 6 : 20} lg={breadcrumbs && breadcrumbs.length === 2 ? 6 : 16} style={{textAlign: breadcrumbs && breadcrumbs.length === 2 ? 'right' : 'center'}}>
       <h1 className="page-title">{ title }</h1>
     </Col>
   </Row>
